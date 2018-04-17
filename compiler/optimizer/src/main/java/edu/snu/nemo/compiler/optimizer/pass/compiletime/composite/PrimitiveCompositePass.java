4143a7d3135c50938a5c6ce6c42cd7665813a917
John Yang <johnyangk@apache.org>
2018-04-17 20:04:15 +0900

    remove used

--- compiler/optimizer/src/main/java/edu/snu/nemo/compiler/optimizer/pass/compiletime/composite/PrimitiveCompositePass.java
+++ compiler/optimizer/src/main/java/edu/snu/nemo/compiler/optimizer/pass/compiletime/composite/PrimitiveCompositePass.java
@@ -33,7 +33,7 @@ public final class PrimitiveCompositePass extends CompositePass {
         new DefaultParallelismPass(), // annotating after reshaping passes, before stage partitioning
         new DefaultStagePartitioningPass(),
         new ReviseInterStageEdgeDataStorePass(), // after stage partitioning
-        new DefaultEdgeUsedDataHandlingPass(),
+        //new DefaultEdgeUsedDataHandlingPass(),
         new ScheduleGroupPass()
         // new CompressionPass()
     ));
