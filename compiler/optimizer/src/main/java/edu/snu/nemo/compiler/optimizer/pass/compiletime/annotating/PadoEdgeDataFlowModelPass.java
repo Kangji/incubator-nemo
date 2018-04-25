634f873fe3f82388909caa5bc9c7b943a83b10f2
JangHo Seo <jangho@jangho.io>
2018-04-25 17:33:28 +0900

    Go Pull

--- compiler/optimizer/src/main/java/edu/snu/nemo/compiler/optimizer/pass/compiletime/annotating/PadoEdgeDataFlowModelPass.java
+++ compiler/optimizer/src/main/java/edu/snu/nemo/compiler/optimizer/pass/compiletime/annotating/PadoEdgeDataFlowModelPass.java
@@ -48,7 +48,7 @@ public final class PadoEdgeDataFlowModelPass extends AnnotatingPass {
       if (!inEdges.isEmpty()) {
         inEdges.forEach(edge -> {
           if (fromTransientToReserved(edge) && !fromSource(edge, dag)) {
-            edge.setProperty(DataFlowModelProperty.of(DataFlowModelProperty.Value.Push));
+            edge.setProperty(DataFlowModelProperty.of(DataFlowModelProperty.Value.Pull));
           } else {
             edge.setProperty(DataFlowModelProperty.of(DataFlowModelProperty.Value.Pull));
           }
