8d0a34e49f6dc76e112cf73bc8989bfedd9340f1
Jangho Seo <jangho@jangho.io>
2018-09-03 09:41:57 +0900

    Remove combiner @ [NEMO-179] Delayed Task Cloning (#112)

--- compiler/frontend/beam/src/main/java/edu/snu/nemo/compiler/frontend/beam/PipelineTranslator.java
+++ compiler/frontend/beam/src/main/java/edu/snu/nemo/compiler/frontend/beam/PipelineTranslator.java
@@ -40,7 +40,6 @@ import java.lang.annotation.*;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Stack;
 import java.util.function.BiFunction;
