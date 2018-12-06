5696e1fa38a9bdfaf773c50266dfd267866286e7
Jangho Seo <jangho@jangho.io>
2018-12-06 17:11:53 +0900

    tidying

--- webui/components/jobs/JobsView.vue
+++ webui/components/jobs/JobsView.vue
@@ -13,7 +13,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 -->
 <template>
-  <el-card>
+  <el-card shadow="never">
     <!--Title-->
     <h1>Nemo Jobs</h1>
 
@@ -103,7 +103,7 @@ limitations under the License.
         </el-table-column>
         <el-table-column label="Progress">
           <template slot-scope="scope">
-            <el-progress :text-inside="true" :stroke-width="18" :percentage="100"></el-progress>
+            <el-progress :text-inside="true" :stroke-width="18" :percentage="100" status="success"></el-progress>
           </template>
         </el-table-column>
       </el-table>
@@ -847,7 +847,7 @@ export default {
             ts.completedTasks += 1
           }
         }
-        ts.progress = ts.totalTasks === 0 ? 0 : (ts.completedTasks / ts.totalTasks) * 100
+        ts.progress = ts.totalTasks === 0 ? 0 : Math.round((ts.completedTasks / ts.totalTasks) * 10000) / 100
       }
       this.$eventBus.$emit('build-table-data', {
         metricId: metric.id,
