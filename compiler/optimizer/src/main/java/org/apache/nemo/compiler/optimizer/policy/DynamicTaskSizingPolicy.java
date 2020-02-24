6656b8f2214f424edf27a495968de0cdabbfdffa
polarcoke2 <polarcoke2@snu.ac.kr>
2020-02-24 12:22:04 +0900

    merged with Dts-unified, but disabled run time pass for experiment

--- compiler/optimizer/src/main/java/org/apache/nemo/compiler/optimizer/policy/DynamicTaskSizingPolicy.java
+++ compiler/optimizer/src/main/java/org/apache/nemo/compiler/optimizer/policy/DynamicTaskSizingPolicy.java
@@ -32,7 +32,7 @@ public final class DynamicTaskSizingPolicy implements Policy {
   public static final PolicyBuilder BUILDER =
     new PolicyBuilder()
       .registerCompileTimePass(new DefaultParallelismPass())
-      .registerRunTimePass(new DynamicTaskSizingRunTimePass(), new SamplingTaskSizingPass())
+      .registerCompileTimePass(new SamplingTaskSizingPass())
       .registerCompileTimePass(new LoopUnrollingPass())
       .registerCompileTimePass(new DefaultEdgeEncoderPass())
       .registerCompileTimePass(new DefaultEdgeDecoderPass())
