b84b9e16a0be6948c48cfcfdfa577716820f78dc
Won Wook SONG <wsong0512@gmail.com>
2020-07-17 03:32:41 +0900

    dir

--- runtime/executor/src/main/java/org/apache/nemo/runtime/executor/data/stores/CrailFileStore.java
+++ runtime/executor/src/main/java/org/apache/nemo/runtime/executor/data/stores/CrailFileStore.java
@@ -71,7 +71,7 @@ public final class CrailFileStore extends AbstractBlockStore implements RemoteFi
     // Distribute $CRAIL_HOME/conf/crail-site.conf to workers, specifying hostname
     this.conf = CrailConfiguration.createConfigurationFromFile();
     this.fs = CrailStore.newInstance(conf);
-    this.fileDirectory = "crail://mem52g-m:9060/";
+    this.fileDirectory = "../tmp_crail";
   }
 
   @Override
