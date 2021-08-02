8058f51e5832e3e16d50d4a05d9982b2111716e2
Kangji <drcotr0623@naver.com>
2021-08-02 21:17:09 +0900

    implemented unit tests

--- compiler/test/src/test/java/org/apache/nemo/compiler/frontend/beam/transform/CombineTransformTest.java
+++ compiler/test/src/test/java/org/apache/nemo/compiler/frontend/beam/transform/CombineTransformTest.java
@@ -22,6 +22,7 @@ import org.apache.beam.runners.core.SystemReduceFn;
 import org.apache.beam.sdk.coders.*;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
 import org.apache.beam.sdk.transforms.Combine;
+import org.apache.beam.sdk.transforms.CombineFnBase;
 import org.apache.beam.sdk.transforms.DoFnSchemaInformation;
 import org.apache.beam.sdk.transforms.display.DisplayData;
 import org.apache.beam.sdk.transforms.windowing.*;
@@ -255,6 +256,172 @@ public class CombineTransformTest {
 
     oc.outputs.clear();
     oc.watermarks.clear();
+  }
+
+  private void clearOutputCollectors(final List<TestOutputCollector> outputCollectors) {
+    for (TestOutputCollector<?> oc : outputCollectors) {
+      oc.outputs.clear();
+      oc.watermarks.clear();
+    }
+  }
+
+  @SuppressWarnings("unchecked")
+  private void processIntermediateCombineElementsPerWindow(final List<WindowedValue> elements,
+                                                           final CombineTransform partialCombineTransform,
+                                                           final CombineTransform intermediateCombineTransform,
+                                                           final CombineTransform finalCombineTransform,
+                                                           final Watermark watermark,
+                                                           final TestOutputCollector<?> ocPartial,
+                                                           final TestOutputCollector<?> ocIntermediate) {
+    for (WindowedValue<?> element : elements) {
+      partialCombineTransform.onData(element);
+    }
+    partialCombineTransform.onWatermark(watermark);
+    for (WindowedValue<?> output: ocPartial.outputs) {
+      intermediateCombineTransform.onData(output);
+    }
+    intermediateCombineTransform.onWatermark(watermark);
+    for (WindowedValue<?> output: ocIntermediate.outputs) {
+      finalCombineTransform.onData(output);
+    }
+    finalCombineTransform.onWatermark(watermark);
+  }
+
+  /**
+   * Test intermediate combine.
+   */
+  @Test
+  @SuppressWarnings("unchecked")
+  public void testIntermediateCombine() {
+    final TupleTag<String> outputTag = new TupleTag<>("main-output");
+    final SlidingWindows slidingWindows = SlidingWindows.of(Duration.standardSeconds(10))
+      .every(Duration.standardSeconds(5));
+    final WindowingStrategy windowingStrategy = WindowingStrategy.of(slidingWindows).withMode(ACCUMULATING_FIRED_PANES);
+
+    final Instant ts1 = new Instant(1000);
+    final Instant ts2 = new Instant(2000);
+    final Instant ts3 = new Instant(6000);
+    final Instant ts4 = new Instant(8000);
+    final Instant ts5 = new Instant(11000);
+    final Instant ts6 = new Instant(14000);
+    final Instant ts7 = new Instant(16000);
+    final Instant ts8 = new Instant(17000);
+    final Instant ts9 = new Instant(19000);
+    final Watermark watermark1 = new Watermark(7000);
+    final Watermark watermark2 = new Watermark(12000);
+    final Watermark watermark3 = new Watermark(18000);
+    final Watermark watermark4 = new Watermark(21000);
+
+    final KvCoder inputCoder = KvCoder.of(STRING_CODER, INTEGER_CODER);
+    final Coder accumulatorCoder;
+    try {
+      accumulatorCoder = combine_fn.getAccumulatorCoder(CoderRegistry.createDefault(), INTEGER_CODER);
+    } catch (CannotProvideCoderException e) {
+      throw new RuntimeException(e);
+    }
+
+    final CombineFnBase.GlobalCombineFn partialCombineFn = new PartialCombineFn(combine_fn, accumulatorCoder);
+    final CombineFnBase.GlobalCombineFn intermediateCombineFn = new IntermediateCombineFn(combine_fn, accumulatorCoder);
+    final CombineFnBase.GlobalCombineFn finalCombineFn = new FinalCombineFn(combine_fn, accumulatorCoder);
+
+    final SystemReduceFn partialSystemReduceFn = SystemReduceFn.combining(STRING_CODER,
+      AppliedCombineFn.withInputCoder(partialCombineFn, CoderRegistry.createDefault(),
+        inputCoder, null, windowingStrategy));
+    final SystemReduceFn intermediateSystemReduceFn = SystemReduceFn.combining(STRING_CODER,
+      AppliedCombineFn.withInputCoder(intermediateCombineFn, CoderRegistry.createDefault(),
+        KvCoder.of(STRING_CODER, accumulatorCoder), null, windowingStrategy));
+    final SystemReduceFn finalSystemReduceFn = SystemReduceFn.combining(STRING_CODER,
+      AppliedCombineFn.withInputCoder(finalCombineFn, CoderRegistry.createDefault(),
+        KvCoder.of(STRING_CODER, accumulatorCoder), null, windowingStrategy));
+
+    final CombineTransformFactory combineTransformFactory = new CombineTransformFactory(
+      inputCoder, new TupleTag<>(), KvCoder.of(STRING_CODER, accumulatorCoder),
+      Collections.singletonMap(outputTag, inputCoder), outputTag, windowingStrategy,
+      PipelineOptionsFactory.as(NemoPipelineOptions.class), partialSystemReduceFn, intermediateSystemReduceFn,
+      finalSystemReduceFn, DoFnSchemaInformation.create(), DisplayData.none());
+
+    final CombineTransform partialCombineTransform = combineTransformFactory.getPartialCombineTransform();
+    final CombineTransform intermediateCombineTransform = combineTransformFactory.getIntermediateCombineTransform();
+    final CombineTransform finalCombineTransform = combineTransformFactory.getFinalCombineTransform();
+
+    // window1 : [-5000, 5000) in millisecond
+    // window2 : [0, 10000)
+    // window3 : [5000, 15000)
+    // window4 : [10000, 20000)
+    List<IntervalWindow> sortedWindows = new ArrayList<>(slidingWindows.assignWindows(ts1));
+    sortedWindows.sort(IntervalWindow::compareTo);
+    final IntervalWindow window1 = sortedWindows.get(0);
+    final IntervalWindow window2 = sortedWindows.get(1);
+    sortedWindows = new ArrayList<>(slidingWindows.assignWindows(ts5));
+    sortedWindows.sort(IntervalWindow::compareTo);
+    final IntervalWindow window3 = sortedWindows.get(0);
+    final IntervalWindow window4 = sortedWindows.get(1);
+
+    // Prepare to test CombineStreamTransform
+    final Transform.Context context = mock(Transform.Context.class);
+    final TestOutputCollector<KV<String, CountFn.Accum>> ocPartial = new TestOutputCollector<>();
+    final TestOutputCollector<KV<String, CountFn.Accum>> ocIntermediate = new TestOutputCollector<>();
+    final TestOutputCollector<KV<String, Integer>> ocFinal = new TestOutputCollector<>();
+    partialCombineTransform.prepare(context, ocPartial);
+    intermediateCombineTransform.prepare(context, ocIntermediate);
+    finalCombineTransform.prepare(context, ocFinal);
+
+    processIntermediateCombineElementsPerWindow(
+      Arrays.asList(
+        WindowedValue.of(KV.of("a", 1), ts1, slidingWindows.assignWindows(ts1), PaneInfo.NO_FIRING),
+        WindowedValue.of(KV.of("c", 1), ts2, slidingWindows.assignWindows(ts2), PaneInfo.NO_FIRING),
+        WindowedValue.of(KV.of("b", 1), ts3, slidingWindows.assignWindows(ts3), PaneInfo.NO_FIRING)),
+      partialCombineTransform, intermediateCombineTransform, finalCombineTransform,
+      watermark1, ocPartial, ocIntermediate);
+    ocFinal.outputs.sort(Comparator.comparing(o -> o.getValue().getKey()));
+
+    Assert.assertEquals(Collections.singletonList(window1), ocFinal.outputs.get(0).getWindows());
+    Assert.assertEquals(2, ocFinal.outputs.size());
+    checkOutput(KV.of("a", 1), ocFinal.outputs.get(0).getValue());
+    checkOutput(KV.of("c", 1), ocFinal.outputs.get(1).getValue());
+    clearOutputCollectors(Arrays.asList(ocPartial, ocIntermediate, ocFinal));
+
+    processIntermediateCombineElementsPerWindow(
+      Arrays.asList(
+        WindowedValue.of(KV.of("a", 1), ts4, slidingWindows.assignWindows(ts4), PaneInfo.NO_FIRING),
+        WindowedValue.of(KV.of("c", 1), ts5, slidingWindows.assignWindows(ts5), PaneInfo.NO_FIRING)),
+      partialCombineTransform, intermediateCombineTransform, finalCombineTransform,
+      watermark2, ocPartial, ocIntermediate);
+    ocFinal.outputs.sort(Comparator.comparing(o -> o.getValue().getKey()));
+
+    Assert.assertEquals(Collections.singletonList(window2), ocFinal.outputs.get(0).getWindows());
+    Assert.assertEquals(3, ocFinal.outputs.size());
+    checkOutput(KV.of("a", 2), ocFinal.outputs.get(0).getValue());
+    checkOutput(KV.of("b", 1), ocFinal.outputs.get(1).getValue());
+    checkOutput(KV.of("c", 1), ocFinal.outputs.get(2).getValue());
+    clearOutputCollectors(Arrays.asList(ocPartial, ocIntermediate, ocFinal));
+
+    processIntermediateCombineElementsPerWindow(
+      Arrays.asList(
+        WindowedValue.of(KV.of("b", 1), ts6, slidingWindows.assignWindows(ts6), PaneInfo.NO_FIRING),
+        WindowedValue.of(KV.of("b", 1), ts7, slidingWindows.assignWindows(ts7), PaneInfo.NO_FIRING),
+        WindowedValue.of(KV.of("a", 1), ts8, slidingWindows.assignWindows(ts8), PaneInfo.NO_FIRING)),
+      partialCombineTransform, intermediateCombineTransform, finalCombineTransform,
+      watermark3, ocPartial, ocIntermediate);
+    ocFinal.outputs.sort(Comparator.comparing(o -> o.getValue().getKey()));
+
+    Assert.assertEquals(Collections.singletonList(window3), ocFinal.outputs.get(0).getWindows());
+    checkOutput(KV.of("a", 1), ocFinal.outputs.get(0).getValue());
+    checkOutput(KV.of("b", 2), ocFinal.outputs.get(1).getValue());
+    checkOutput(KV.of("c", 1), ocFinal.outputs.get(2).getValue());
+    clearOutputCollectors(Arrays.asList(ocPartial, ocIntermediate, ocFinal));
+
+    processIntermediateCombineElementsPerWindow(
+      Arrays.asList(WindowedValue.of(KV.of("c", 3), ts9, slidingWindows.assignWindows(ts9), PaneInfo.NO_FIRING)),
+      partialCombineTransform, intermediateCombineTransform, finalCombineTransform,
+      watermark4, ocPartial, ocIntermediate);
+    ocFinal.outputs.sort(Comparator.comparing(o -> o.getValue().getKey()));
+
+    Assert.assertEquals(Collections.singletonList(window4), ocFinal.outputs.get(0).getWindows());
+    checkOutput(KV.of("a", 1), ocFinal.outputs.get(0).getValue());
+    checkOutput(KV.of("b", 2), ocFinal.outputs.get(1).getValue());
+    checkOutput(KV.of("c", 4), ocFinal.outputs.get(2).getValue());
+    clearOutputCollectors(Arrays.asList(ocPartial, ocIntermediate, ocFinal));
   }
 
   // Test with late data
